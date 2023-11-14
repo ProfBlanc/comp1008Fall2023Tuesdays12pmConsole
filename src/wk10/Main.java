a096a615c285bb60f2335107f6c65b6b5e5a5128
ProfBlanc <34591333+ProfBlanc@users.noreply.github.com>
2023-11-14 14:00:10 -0500

    Update Main.java

--- src/wk10/Main.java
+++ src/wk10/Main.java
@@ -1,6 +1,116 @@
 // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
 
+abstract class Shape{
+
+    private int sides;
+    private String name="Shape";
+    double length, width, radius, diameter;
+    
+    public int getSides(){
+        return sides;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setSides(int sides){
+        this.sides = sides;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public Shape(){}
+    public Shape(String name, int sides){
+        setName(name);
+        setSides(sides);
+    }
+    public Shape(String name, int sides, double length, double with){
+        setName(name);
+        setSides(sides);
+        this.length = length;
+        this.width = width;
+    }
+    
+    //abstract method
+    public abstract double getPerimeter();
+    public abstract double getArea();
+    public abstract String coolMethod(int a, double b);
+}
+
+abstract class ThreeDShape extends Shape{}
+
+class Rectangle extends Shape{
+    
+     @Override
+    public double getArea(){
+        return length * width;
+    }
+    @Override
+    public double getPerimeter(){
+        return 2 * (length + width);
+    }
+    @Override
+    public String coolMethod(int a, double b){
+        return a + "-" + b;
+    }
+    
+}
+
+interface Screen{
+    //instance variables => mis nommer => constant
+    int MAX_WIDTH = 1000, MAX_HEIGHT = 2000; 
+    
+    //what action(s) does this entity (Screen) NEED to implement?
+    
+    boolean isTooHot(double externalTemperature);
+    /**
+     * called when device needs to automatically shut down
+     * @param statusCode status code of reason
+     * @return a message of why screen shut dow
+     */ 
+    String autoShutDown(int statusCode);
+    
+}
+
+interface Face{
+    
+    String VALUE = "Hello";
+    
+    void myMethod();
+}
+
+interface ISample1 extends Face{}
+
+
+interface ISample2 extends Screen, Face{}
+
+
+class SampleClass implements Screen, Face{
+    @Override
+    public void myMethod(){}
+     @Override
+    public boolean isTooHot(double externalTemperature){
+        return false;
+    }
+    @Override
+    public String autoShutDown(int statusCode){
+        return "goodbye";
+    }
+}
+
+class Cellphone extends Rectangle implements Screen{
+    
+    @Override
+    public boolean isTooHot(double externalTemperature){
+        return false;
+    }
+    @Override
+    public String autoShutDown(int statusCode){
+        return "goodbye";
+    }
+}
+
+
 class HelloWorld {
     
     static void example1(){
